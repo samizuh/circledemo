b6f6b3ad1ab4be303bfed366f970a15ea3c329a8
Sami Z <samiz@google.com>
2018-03-04 21:20:11 -0800

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -6,7 +6,7 @@ import (
   
 func TestUpper(t *testing.T) {
 	incoming := "Test String"
-	expected := "TEST STRINGx"
+	expected := "TEST STRING"
 	actual := ToUpper(incoming)
   
 	if actual != expected {
